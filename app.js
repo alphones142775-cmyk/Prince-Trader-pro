063960d85d276f673d08466cd80a745ae95430c7
Alphones Odhiambo <alphones142775@gmail.com>
2026-05-30 02:36:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,74 @@
 // Sell Trade
 // Update Balance
 // Load Trade History
+// BUY CALL
+document.getElementById(
+  "buyCall"
+).onclick = async () => {
+
+  const stake = Number(
+    document.getElementById(
+      "stake"
+    ).value
+  );
+
+  // REQUEST PROPOSAL
+  connection.send(
+    JSON.stringify({
+
+      proposal:1,
+
+      amount: stake,
+
+      basis:"stake",
+
+      contract_type:"CALL",
+
+      currency:"USD",
+
+      duration:1,
+
+      duration_unit:"t",
+
+      symbol:"R_75"
+
+    })
+  );
+
+};
+
+
+// BUY PUT
+document.getElementById(
+  "buyPut"
+).onclick = async () => {
+
+  const stake = Number(
+    document.getElementById(
+      "stake"
+    ).value
+  );
+
+  connection.send(
+    JSON.stringify({
+
+      proposal:1,
+
+      amount: stake,
+
+      basis:"stake",
+
+      contract_type:"PUT",
+
+      currency:"USD",
+
+      duration:1,
+
+      duration_unit:"t",
+
+      symbol:"R_75"
+
+    })
+  );
+
+};
